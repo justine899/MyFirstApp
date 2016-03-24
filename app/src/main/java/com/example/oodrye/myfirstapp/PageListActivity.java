1b7257f11800c7e268f2cb8e44045a9a24bb821a
justine899 <justine899@yahoo.com>
2016-03-24 09:24:26 -0700

    Hello Wurld Mail

--- app/src/main/java/com/example/oodrye/myfirstapp/PageListActivity.java
+++ app/src/main/java/com/example/oodrye/myfirstapp/PageListActivity.java
@@ -49,7 +49,7 @@ public class PageListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hello Wurld", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
